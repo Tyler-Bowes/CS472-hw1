64dc74e8938fa41bb6247bd500d51fe3c4f080e0
Tyler Bowes <tylerbowes@u.boisestate.edu>
2024-02-22 11:55:29 -0700

    test draw

--- Hw1/main.java
+++ Hw1/main.java
@@ -7,7 +7,7 @@ public class main {
     public static void main(String[] args) {
 	Window window0=new SwingWindow("Lexi 0");
     Glyph rect1 = new Rectangle(20, 10);
-    rect1.draw(window0);
+    // rect1.draw(window0);
     // window0.drawRectangle(0, 0, 20, 10);
     // Glyph a = new Character('a');
     // window0.setContents(rect1);
