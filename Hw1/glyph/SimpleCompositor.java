76fbc9d601b8ca61f5863f5dce2af3a70e92ad04
Tyler Bowes <tylerbowes@u.boisestate.edu>
2024-02-23 21:22:17 -0700

    idk

--- Hw1/glyph/SimpleCompositor.java
+++ Hw1/glyph/SimpleCompositor.java
@@ -18,24 +18,22 @@ public class SimpleCompositor implements Compositor{ // cannont extend b/c it's 
     public void compose() {
         // create cursor based on parent
         Bounds cursor = composition.getbounds();
-        Glyph child = null;
+        Glyph child;
         // int index = 0;
         try {
             // for (... child= ...) {
             // for (Glyph child : composition.getChildren()) {
             for (int index = 0; index < composition.getChildren().size(); index++) {
-                if(composition.getChild(index) != null) {
-                    child = composition.getChild(index);
-                    // ask child to set size, based on window
-                    child.setSize(window);
-                    // ask child to set position, based on cursor
-                    child.setPosition(cursor);
-                    // ask child to compose itself, recursively
-                    child.compose();
-                    // ask parent to adjust itself and cursor, based on child
-                    composition.moveBounds(cursor, child);
-                    // System.out.println("Index: " + index + "Cursor: " + cursor.getX() + ", " + cursor.getY() + ", " + cursor.getWidth() + ", " + cursor.getHeight());
-                }
+                child = composition.getChild(index);
+                // ask child to set size, based on window
+                child.setSize(window);
+                // ask child to set position, based on cursor
+                child.setPosition(cursor);
+                // ask child to compose itself, recursively
+                child.compose();
+                // ask parent to adjust itself and cursor, based on child
+                cursor = composition.moveBounds(cursor, child);
+                // System.out.println("Index: " + index + "Cursor: " + cursor.getX() + ", " + cursor.getY() + ", " + cursor.getWidth() + ", " + cursor.getHeight());
             }
             // ask parent to adjust itself, based on cursor
             composition.adjustBounds(cursor);
@@ -44,14 +42,6 @@ public class SimpleCompositor implements Compositor{ // cannont extend b/c it's 
         } catch (Exception e) {
             System.out.println("Error when running compose method!: " + e);
         }
-        
-
-        // ask parent to adjust itself, based on cursor
-        // if (composition.getChild(0) != null) {
-        //     Bounds parent_bounds = composition.getChild(0).getbounds();
-        //     composition.getbounds().setBounds(parent_bounds.getX(), parent_bounds.getY(), 
-        //             parent_bounds.getWidth(), parent_bounds.getHeight());
-        // }
     }
     
 }
